70a5624e04618a261c6bcfb3eab47dbf0677eb83
PaveL <BPaveL07@yandex.ru>
2021-10-14 21:39:29 +0300

    Initial commit

---
+++ src/JavaApplication32.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.net.Socket;
+
+public class JavaApplication32 {
+    public static void main(String[] args) throws IOException {
+
+        Socket soc = new Socket("localhost", 30333);
+
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(soc.getOutputStream()));
+
+        writer.write("Hello Server!");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(soc.getInputStream()));
+        String str = reader.readLine();
+        System.out.println("Server sent:" + str);
+
+    }
+}
